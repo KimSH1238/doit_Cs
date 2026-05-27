53fd9cfe39e0c5f9856a43cd83cc79932aeb2ad6
KimSH <36595569+KimSH1238@users.noreply.github.com>
2026-05-27 21:21:10 +0900

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("A B C");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
