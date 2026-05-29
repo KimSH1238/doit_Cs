c66d7a45f16c84e7672794fda5a228fe6f443c69
KimSH <36595569+KimSH1238@users.noreply.github.com>
2026-05-29 18:30:06 +0900

    Add files via upload

---
+++ 51p_fori/51p_fori/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class MainClass1
+{
+    public static void Main(string[] args)
+    {
+        for (int i = 0; i < 11; i++)
+            Console.WriteLine(i);
+    }
+}
+
+
+
---
+++ 53p_whilei/53p_whilei/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+class MainClass2
+{
+    public static void Main(string[] args)
+    {
+        int i = 0;
+        while (i < 11)
+            Console.WriteLine(i++);
+        Console.WriteLine("\nHello, World!\n");
+    }
+}
+
