cabe747d43f3f5de62acc13c803e2be439ea6208
KimSH <36595569+KimSH1238@users.noreply.github.com>
2026-05-29 15:29:42 +0900

    Add files via upload

---
+++ 44p_variables/44p_variables/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class MainClass
+{
+    public static void Main(String[] args)
+    {
+        bool boolVari = true;
+        int intVari = 10;
+        float floatVari = 3.4F;
+        char charVari = 'a';
+
+        Console.WriteLine(boolVari);
+        Console.WriteLine(intVari);
+        Console.WriteLine(floatVari);
+        Console.WriteLine(charVari);
+
+        Console.WriteLine("\nHello, World!\n");
+    }
+}
