a91bcf3a2f5181b5600b8ca9348335f4ac1a98f0
KimSH <36595569+KimSH1238@users.noreply.github.com>
2026-05-30 16:36:57 +0900

    Add files via upload

---
+++ 76p_constructor/76p_constructor/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.Contracts;
+
+using System;
+
+class Cat
+{
+    public string Name = null;
+
+    public Cat(string name)
+    {
+        Name = name;
+        Console.WriteLine("고양이의 이름은 " + Name + "이다.");
+    }
+}
+
+class MainClass
+{ 
+    public static void Main(string[] args)
+    {
+        Cat coco = new Cat("코코");
+        Cat moly = new Cat("몰리");
+
+        Console.WriteLine("\nHello, World!\n");
+    }
+}
