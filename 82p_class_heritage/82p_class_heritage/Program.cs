41e8127e3a7f00341d406f9850612a2d7c053edc
KimSH <36595569+KimSH1238@users.noreply.github.com>
2026-05-30 20:55:26 +0900

    Add files via upload

---
+++ 82p_class_heritage/82p_class_heritage/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+class Robot
+{
+    public void Move()
+    {
+        Console.WriteLine("로봇이 움직입니다.");
+    }
+}
+class CleanRobot : Robot
+{
+    public void Clean()
+    {
+        Console.WriteLine("청소를 시작합니다.");
+    }
+}
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        CleanRobot cleanRobot = new CleanRobot();
+        cleanRobot.Move();
+ // CleanRobot 을 호출했는데, 상속으로 Robot 까지 호출된 모습
+        cleanRobot.Clean();
+    }
+}
