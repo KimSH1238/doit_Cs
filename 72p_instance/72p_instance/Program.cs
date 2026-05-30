c489fced3b3f8d2ecd7a2da70fba8c370cbcd03e
KimSH <36595569+KimSH1238@users.noreply.github.com>
2026-05-30 15:55:30 +0900

    Add files via upload

---
+++ 72p_instance/72p_instance/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Person
+{
+    public string Name = null;
+    public string Birthday = null;
+    public string Gender = null;
+
+    public void Eat()
+    {
+        Console.WriteLine(Name + "이 아침을 먹습니다.");
+    }
+    public void Walk()
+    {
+        Console.WriteLine(Name + "이 거리를 걷습니다.");
+    }
+}
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Person p1;
+        p1 = new Person();
+        p1.Name = "ksh";
+
+        p1.Eat();
+        p1.Walk();
+
+        Console.WriteLine("\nHello, World!\n");
+    }
+}
---
+++ gemini_input/gemini_input/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("이름을 입력하시오 :");
+        //사용자가 입력한 문자열을 변수에 저장
+        string input = Console.ReadLine();
+
+        Console.WriteLine($"입력값을 전달받았습니다. {input}님");
+        Console.WriteLine("\nHello, World!\n");
+    }
+}
+,
