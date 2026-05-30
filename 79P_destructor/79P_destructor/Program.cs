85f39908ecd65068dc03fc7f30c66ade6eeaa33e
KimSH <36595569+KimSH1238@users.noreply.github.com>
2026-05-30 20:21:46 +0900

    Add files via upload

---
+++ 79P_destructor/79P_destructor/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+class Cat
+{
+    public string Name = null;
+
+    public Cat(string name)
+    {
+        Name = name;
+        Console.WriteLine("고양이의 이름은 " + Name + " 이다.");
+    }
+    ~Cat()
+    {
+        Console.WriteLine(Name + "가 사라집니다.");
+    }
+}
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+ // 블록을 만들어, 고양이 속성들의 수명을 제한합니다. --> 실행 오류
+ // 블록을 만들어, 고양이 속성들의 수명을 제한합니다. --> 실행 오류
+        {
+            Cat coco = new Cat("코코");
+            Cat moly = new Cat("몰리");
+        }
+    }
+//        GC.Collect();
+//        GC.WaitForPendingFinalizers();
+}
