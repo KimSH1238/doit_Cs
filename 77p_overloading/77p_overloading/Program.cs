c88b0f91dea351abdd008881d4ccea49fcbd75cb
KimSH <36595569+KimSH1238@users.noreply.github.com>
2026-05-30 17:15:43 +0900

    Add files via upload

---
+++ 77p_overloading/77p_overloading/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Cat
+{
+    public string Name = null;
+    public int Weight = 0;
+
+    public Cat(string name)
+    {
+        Name = name;
+        Console.WriteLine("고양이의 이름은 :\n" + Name + "입니다.");
+
+        Console.WriteLine("\nHello, World!\n");
+    }
+    public Cat(string name, int weight)
+    {
+        Name = name;
+        Weight = weight;
+        Console.WriteLine("고양이의 이름은 :n" + Name + "이며,\n체중은 :" + Weight + "입니다.");
+
+        Console.WriteLine("\nHello, World!\n");
+    }
+}
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Cat coco = new Cat("코코");
+        Cat moly = new Cat("몰리", 3);
+    }
+}
---
+++ gemini_constructor_input/gemini_constructor_input/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Cat
+{
+    public string Name = null;
+    
+    // 생성자 : 고양이가 만들어질때 이름을 지정한다.
+    public Cat(string name)
+    {
+        Name = name;
+        Console.WriteLine("고양이의 이름은 " + name + " 이다.");
+    }
+}
+
+class Mainclass
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("첫번째 고양이의 이름을 지정하시오 :\n");
+        string firstName = Console.ReadLine();
+        Cat cat1 = new Cat(first[pppppppppppName);
+
+        // 줄바꿈을 위한 빈, 한줄 출력
+        Console.WriteLine();
+
+        Console.Write("두번째 고양이의 이름을 지정하시오 :\n");
+        string secondName = Console.ReadLine();
+        Cat cat2 = new Cat(secondName);
+
+        Console.WriteLine("\nHello World\n");
+    }
+}
