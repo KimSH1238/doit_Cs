819f11dcec21be39fe75bfe272d6c4fcfcd830e7
KimSH <36595569+KimSH1238@users.noreply.github.com>
2026-05-30 22:31:14 +0900

    Add files via upload

---
+++ PrintArray_utube/PrintArray_utube/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace PrintingArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[30];
+            for (int i=0; i<n.Length; i++)
+            {
+                n[i] = i + 1;
+            }
+            Printarray(n)
+        }
+        static void Printarray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                Console.WriteLine(arr[i])
+        }
+    }
+}
