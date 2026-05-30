56612298a03255b1c1cf5daab7618a91b9f2d2e6
KimSH <36595569+KimSH1238@users.noreply.github.com>
2026-05-30 16:01:51 +0900

    Add files via upload

--- 75p_constructor/75p_constructor/Program.cs
+++ 75p_constructor/75p_constructor/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+
+class Cat
+{
+    public string Name = null;
+
+    public Cat()
+    {
+        Console.WriteLine("생성자가 호출되었습니다.");
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Cat myCat = new Cat();
+            Console.WriteLine("\nHello, World!\n");
+        }
+    }
+}
