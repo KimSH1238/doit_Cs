eeb321e91963a9af827e0d87a9448b3cd18e869d
KimSH <36595569+KimSH1238@users.noreply.github.com>
2026-05-30 22:15:58 +0900

    Add files via upload

---
+++ ArrayTest_utube/ArrayTest_utube/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace ArrayTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[30];
+            for (int i = 0; i < n.Length; i++)
+            {
+                n[i] = i + 1;
+                Console.WriteLine(n[i]);
+            }
+        }
+    }
+}
