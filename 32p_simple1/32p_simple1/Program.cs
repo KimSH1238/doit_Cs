52d6a096c0fd5b6c9e82456e03c51a7ccf0dea45
KimSH <36595569+KimSH1238@users.noreply.github.com>
2026-06-01 14:24:31 +0900

    Add files via upload

---
+++ 32p_simple1/32p_simple1/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("입력하고 싶은 항의 개수를 입력하시오 : ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        if (b<10 && b>0)
+        {
+            int[] a = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            for (int i = 0; i < b; i++)
+            {
+                Console.Write(a[i]);
+            }
+            Console.WriteLine("");
+        }
+        else
+        {
+            Console.WriteLine("입력오류");
+        }
+    }
+}
