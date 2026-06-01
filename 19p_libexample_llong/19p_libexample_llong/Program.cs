12e60c952771d850f52380e1ac3d60ac84229a9c
KimSH <36595569+KimSH1238@users.noreply.github.com>
2026-06-01 12:42:13 +0900

    Add files via upload

---
+++ 19p_libexample_llong/19p_libexample_llong/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, Action<TextWriter>> dic = new Dictionary<string, Action<TextWriter>>();
+        // 현재 줄이 var을 사용하던 코드
+        dic.Add("sample1", (writer) => { writer.WriteLine("I'm sample1!"); });
+        dic.Add("sample2", (writer) => { writer.WriteLine("I'm sample2!"); });
+        foreach (var item in dic.Values)
+        {
+            item(Console.Out);
+        }
+    }
+}
