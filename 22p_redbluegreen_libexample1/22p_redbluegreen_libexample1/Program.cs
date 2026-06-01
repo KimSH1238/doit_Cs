c98b067a3df858b7cc9d8bf5ad366efe9b448e61
KimSH <36595569+KimSH1238@users.noreply.github.com>
2026-06-01 13:16:39 +0900

    Add files via upload

---
+++ 22p_redbluegreen_libexample1/22p_redbluegreen_libexample1/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("빨강은 1, 파랑은 2, 초록은 3을 입력하시오 : ");
+        // 제일 중요한 부분 : 사용자의 입력을 문자열로 받기 (정수형 변수로 받을 필요가 없음)
+        string input = Console.ReadLine();
+        switch (input)
+        {
+            case "1":
+                Console.WriteLine("빨강");
+                break;
+            case "2":
+                Console.WriteLine("파랑");
+                break;
+            case "3":
+                Console.WriteLine("초록");
+                break;
+            default:
+                Console.WriteLine("입력값이 잘못되었다.");
+                break;
+        }
+    }
+}
---
+++ 22p_redbluegreen_libexample2/22p_redbluegreen_libexample2/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("red, green, blue 중 하나를 1,2,3 중 하나로 입력하시오 : ");
+        string input = Console.ReadLine();
+        convert(ref input);
+        Console.WriteLine(input);
+    }
+    static void convert(ref string color)
+    // 이 부분이 검토가 필요한 부분
+    {
+        if (color == "1")
+            color = "빨강";
+        else if (color == "2")
+            color = "파랑";
+        else if (color == "3")
+            color = "초록";
+        else
+            color = "입력값이 잘못되었다.";
+    }
+}
