b5115eb2a52f08096c5d31f203a8de1be232e8df
KimSH <36595569+KimSH1238@users.noreply.github.com>
2026-06-01 15:46:35 +0900

    Add files via upload

---
+++ 32p_simple3/32p_simple3/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using System.ComponentModel.Design;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("원하는 배열 해당 개수를 입력하시오 : ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        if (b <= 10 && b > 0)
+        {
+            int[] a = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int count = 0;
+ // 여기서부터 foreach 문을 넣어본다.
+            foreach (var item in a)
+            {
+                if(count>=b)
+                {
+                    break;
+                }
+                Console.Write(item);
+                count++;
+            }
+            Console.WriteLine("");
+        }
+        else
+        {
+            Console.WriteLine("입력 오류");
+        }
+    }
+}
