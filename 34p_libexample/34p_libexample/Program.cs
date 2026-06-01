7c8a6827eae99003e45e78c6584de779d1af35e6
KimSH <36595569+KimSH1238@users.noreply.github.com>
2026-06-01 18:39:17 +0900

    Add files via upload

---
+++ 34p_libexample/34p_libexample/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var heavyQuery = Enumerable.Range(0, 10).Where(c =>
+        {
+            Task.Delay(300).Wait();
+ // 실제 업무에서는 무거운 처리를 하고 있다.
+            return true;
+        });
+        var start = DateTime.Now;
+        var enumerator = heavyQuery.GetEnumerator();
+        for(; enumerator.MoveNext(); )
+ // 좌 우를 빼고, 가운데 만으로 for문을 구성할 수 있다.
+        {
+            Console.Write(enumerator.Current);
+        }
+        Console.WriteLine("\n소요시간 : {0}", DateTime.Now - start);
+    }
+}
