e5f57e8f48f095c9285a3f54b21147737871816f
KimSH <36595569+KimSH1238@users.noreply.github.com>
2026-06-01 11:44:44 +0900

    Add files via upload

---
+++ 31p_libexample_gemini/31p_libexample/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var heavyQuery = Enumerable.Range(0, 10).Where(c =>{
+ // 람다 화살표 암기할것
+            Task.Delay(1000).Wait();
+ // 실제 업무에서는 무거운 처리를 하고 있을것
+            return true;
+        } ).ToList();
+
+        var start = DateTime.Now;
+        for (int i = 0; i < heavyQuery.Count; i++)
+        {
+            Console.WriteLine(heavyQuery[i].ToString());
+        }
+        Console.WriteLine("소요시간 :{0}", DateTime.Now - start);
+    }
+}
