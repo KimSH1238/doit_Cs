63c317778963bded1fa3cca8ce4ddfee9bdc83b4
KimSH <36595569+KimSH1238@users.noreply.github.com>
2026-06-14 11:22:06 +0900

    Add files via upload

---
+++ 40p/40p/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 1;
+        while (a==1)
+        {
+            int b = a * 2;
+            if (b > 0)
+                break;
+            Console.WriteLine(b);
+            break;
+        }
+        Console.WriteLine("All done");
+    }
+}
