d3ccab9fe35510d146cd4d0998807116b8d0fe3e
KimSH <36595569+KimSH1238@users.noreply.github.com>
2026-06-14 11:43:35 +0900

    Add files via upload

---
+++ 45p_1_bool/45p_1_bool/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+class Program
+{
+    private static void sample(int a)
+    {
+        bool first = true;
+        while (a >= 0 || first)
+        {
+            Console.Write(a--);
+            first = false;
+        }
+    }
+    static void Main(string[] args)
+    {
+        sample(10);
+        sample(-1);
+        Console.WriteLine();
+    }
+}
