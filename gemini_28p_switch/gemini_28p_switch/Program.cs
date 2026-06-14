f20d5b4f2f6feea2161f34d15ff8b6cc1ce0f1ee
KimSH <36595569+KimSH1238@users.noreply.github.com>
2026-06-14 10:07:26 +0900

    Add files via upload

---
+++ gemini_28p_switch/gemini_28p_switch/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+class Program
+{
+    static void convert(ref string color)
+    {
+        switch (color)
+        {
+            case "Red":
+                color = "빨강";
+                break;
+            case "Green":
+                color = "초록";
+                break;
+            case "Blue":
+                color = "파랑";
+                break;
+            default:
+                color = "알 수 없는 색";
+                break;
+        }
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Red, Blue, Green 중 하나를 입력 : ");
+        string s = Console.ReadLine();
+        // 원래 여기서 입력을 받아야 하는 부분
+        convert(ref s);
+        Console.WriteLine(s);
+    }
+}
