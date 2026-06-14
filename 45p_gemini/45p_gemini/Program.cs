e08e5fbe01399224e16767b95c32b0cf1bac2f1d
KimSH <36595569+KimSH1238@users.noreply.github.com>
2026-06-14 12:13:13 +0900

    Add files via upload

---
+++ 45p_gemini/45p_gemini/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+class Program
+{
+    private static void sample(int a)
+    {
+        bool first = true;
+        while (a >= 0 || first)
+        {
+            Console.WriteLine(a--);
+            first = false;
+        }
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("시작할 정수를 입력하시오 : ");
+        int inputNumber = Convert.ToInt32(Console.ReadLine());
+
+        sample(inputNumber);
+        sample(-1);
+        Console.WriteLine();
+    }
+}
